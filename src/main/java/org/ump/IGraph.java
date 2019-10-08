87194ac369a08151d1b486993b3f02449b99a812
Mikhail Piankov <pyankovmu@ya.ru>
2019-10-08 09:27:33 -0400

    format

--- src/main/java/org/ump/IGraph.java
+++ src/main/java/org/ump/IGraph.java
@@ -4,30 +4,30 @@ import java.util.List;
 
 public interface IGraph<T> {
 
-    /* Add vertex with specified type to existing graph
-    *  Vertex has to be uniqe objects
-    *  If vertex already exists it will not to be added
-    * */
+    /*  Add vertex with specified type to existing graph
+     *  Vertex has to be uniqe objects
+     *  If vertex already exists it will not to be added
+     * */
     void addVertex(T vertex);
 
-    /* Add precreated edge to graph
-    *  Each edge contains Objects "From" and "To" whose expected to be on graph
-    *  for directed graph edge will be added only to vertex "From"
-    *  for undirected will be created two edges and attached to both
-    * */
+    /*  Add precreated edge to graph
+     *  Each edge contains Objects "From" and "To" whose expected to be on graph
+     *  for directed graph edge will be added only to vertex "From"
+     *  for undirected will be created two edges and attached to both
+     * */
     void addEdge(Edge<T> edge);
 
-    /* Add edge between two vertices
-    *  both vertices has to be exists in praph
-    *  Otherwise you will see warning in STDERR
-    *  @@ "addEdge: Vertex 'from/to' not founded"
-    * */
+    /*  Add edge between two vertices
+     *  both vertices has to be exists in praph
+     *  Otherwise you will see warning in STDERR
+     *  @@ "addEdge: Vertex 'from/to' not founded"
+     * */
     void addEdge(T from, T to);
 
-    /* Find path between two vertices
-     * both vertices has to be exists on graph
-     * otherwise you will see warning
-     * @@ "getPath: Vertex 'to' not founded"
+    /*  Find path between two vertices
+     *  both vertices has to be exists on graph
+     *  otherwise you will see warning
+     *  @@ "getPath: Vertex 'to' not founded"
      * */
     List<Edge<T>> getPath(T from, T to);
 }
